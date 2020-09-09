95abd5b0408fe29746bfb932b7b23e4b57f817bc
sunimalherath <sunimaldv@gmail.com>
2020-09-09 14:27:11 +1000

    adding onChange event to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
-    render() {
-        return(
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" />
-                    </div>                    
-                </form>
-            </div>
-        );
-    }
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    // if we use onInputChange with () below, then it will be called everytime the component render
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
