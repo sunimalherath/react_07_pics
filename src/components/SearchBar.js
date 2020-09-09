0d20ade452b5a891d61a5c9f2b0e6ae50f658037
sunimalherath <sunimaldv@gmail.com>
2020-09-09 14:40:40 +1000

    using controlled event for searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // if we use this way with the onInputChange function, the <input /> element is uncontrolled.
   onInputChange(event) {
     console.log(event.target.value);
   }
+
+  // to make the <input /> controlled,
+  // 1. define state
+  state = { term: "" };
 
   render() {
     // if we use onInputChange with () below, then it will be called everytime the component render
@@ -12,7 +17,12 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            {/* <input type="text" onChange={this.onInputChange} /> */}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
